5e81887a62fb1462ec0556d868569c123e1fdb9c
Salino3 <flaviobcn012@gmail.com>
2026-06-06 16:19:42 +0200

    add endpoint call

--- src/pods/characters/characters.component.tsx
+++ src/pods/characters/characters.component.tsx
@@ -1,7 +1,28 @@
 import type React from "react";
+import { useEffect } from "react";
+import { constants } from "../../config/constants";
 import "./characters.styles.scss";
 
 export const Characters: React.FC = () => {
+  //
+  async function getCharacters(id: string) {
+    try {
+      const response = await fetch(constants.CHARACTERS + id);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      console.log(result);
+    } catch (error: any) {
+      console.error(error?.message);
+    }
+  }
+
+  useEffect(() => {
+    getCharacters("");
+  }, []);
+
   return (
     <div className="rootCharacters">
       <h1>Characters!</h1>
