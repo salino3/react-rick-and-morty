f8631267964e05ee34a76c922d8ae16f9fbb5d66
Salino3 <flaviobcn012@gmail.com>
2026-06-06 15:56:00 +0200

    placed anchors

--- src/pods/home/home.component.tsx
+++ src/pods/home/home.component.tsx
@@ -1,10 +1,22 @@
 import type React from "react";
+import { Link } from "react-router-dom";
+import { routesApp } from "../../router/interface";
 import "./home.styles.scss";
 
 export const Home: React.FC = () => {
   return (
     <div className="rootHome">
       <h1>Home!</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to={routesApp.root}>Home</Link>
+          </li>
+          <li>
+            <Link to={routesApp.characters}>Characters</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
