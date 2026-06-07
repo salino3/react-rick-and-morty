5e353e945e0a7dce5c8773f3c88db123e900b04d
Salino3 <flaviobcn012@gmail.com>
2026-06-07 10:38:14 +0200

    add second carrousel

--- src/pods/characters/characters.component.tsx
+++ src/pods/characters/characters.component.tsx
@@ -18,6 +18,7 @@ import "./characters.styles.scss";
 
 export const Characters: React.FC = () => {
   const swiperRef = useRef<SwiperType | null>(null);
+  const swiperRef02 = useRef<SwiperType | null>(null);
 
   const [nameCharacter, setNameCharacter] = useState<string>("");
   const [resultData, setResultData] = useState<ResultCharacters[]>([]);
@@ -52,7 +53,7 @@ export const Characters: React.FC = () => {
     autoplay: {
       delay: 0, // 0 delay forces it to immediately move to the next slide
       disableOnInteraction: false, // Prevents user interactions from permanently killing autoplay
-      // pauseOnMouseEnter: true, // ⏸️ Pauses the movement when mouse hovers over the container
+      pauseOnMouseEnter: false, // ⏸️ Pauses the movement when mouse hovers over the container
     },
     speed: 6000, // 6000ms = 6 seconds per slide transition. Increase this number to make it even slower and smoother!
 
@@ -74,7 +75,55 @@ export const Characters: React.FC = () => {
 
   const handleMouseLeave = () => {
     if (swiperRef.current) {
+      swiperRef.current.autoplay.paused = false;
       swiperRef.current.autoplay.start();
+    }
+  };
+
+  // swiperSettings02
+  const swiperSettings02: SwiperProps = {
+    // Load the pagination dot module
+    modules: [Pagination, Autoplay],
+    // Enable clickable pagination dots
+    pagination: { clickable: true },
+    // 🔄 TRUE INFINITE LOOP EFFECT ACTIVATED!
+    loop: true,
+    // Number of cards visible at the same time
+    slidesPerView: 2,
+    // Space between cards (in pixels)
+    spaceBetween: 20,
+    // Allow touch/drag dragging
+    allowTouchMove: true,
+
+    // Configure 'Autoplay' for continuous crawl and hover pause
+    autoplay: {
+      delay: 0, // 0 delay forces it to immediately move to the next slide
+      disableOnInteraction: false, // Prevents user interactions from permanently killing autoplay
+      pauseOnMouseEnter: false, // ⏸️ Pauses the movement when mouse hovers over the container
+      reverseDirection: true,
+    },
+    speed: 6000, // 6000ms = 6 seconds per slide transition. Increase this number to make it even slower and smoother!
+
+    //  Set the duration of the transition between slides (in milliseconds)
+    className: "mySwiper",
+
+    // Capture the instance when Swiper initializes
+    onSwiper: (swiper) => {
+      swiperRef02.current = swiper;
+    },
+  };
+
+  //
+  const handleMouseEnter02 = () => {
+    if (swiperRef02.current) {
+      swiperRef02.current.autoplay.stop();
+    }
+  };
+
+  const handleMouseLeave02 = () => {
+    if (swiperRef02.current) {
+      swiperRef02.current.autoplay.paused = false;
+      swiperRef02.current.autoplay.start();
     }
   };
 
@@ -90,7 +139,25 @@ export const Characters: React.FC = () => {
           <strong>There are no Characters</strong>
         ) : (
           <Swiper {...swiperSettings}>
-            {resultData.map((data: ResultCharacters) => (
+            {resultData.slice(0, 10).map((data: ResultCharacters) => (
+              <SwiperSlide key={data.id}>
+                <CardCharacter data={data} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        )}
+      </div>
+      {/*  */}
+      <div
+        className="carouselResultsWrapper02"
+        onMouseEnter={handleMouseEnter02}
+        onMouseLeave={handleMouseLeave02}
+      >
+        {resultData.length === 0 ? (
+          <strong>There are no Characters</strong>
+        ) : (
+          <Swiper {...swiperSettings02}>
+            {resultData.slice(10, 20).map((data: ResultCharacters) => (
               <SwiperSlide key={data.id}>
                 <CardCharacter data={data} />
               </SwiperSlide>
