8e2798c0e9a878e949167d1fdda288bfc100669e
Salino3 <flaviobcn012@gmail.com>
2026-06-07 11:38:32 +0200

    add hook

---
+++ src/hooks/use-resize-observer.tsx
@@ -0,0 +1,40 @@
+import { useState, useEffect, useRef, MutableRefObject } from "react";
+
+interface Dimensions {
+  width: number;
+  height: number;
+}
+
+export function useResizeObserver<T extends HTMLElement>(): [
+  MutableRefObject<T | null>,
+  Dimensions,
+] {
+  const ref = useRef<T | null>(null);
+  const [dimensions, setDimensions] = useState<Dimensions>({
+    width: 0,
+    height: 0,
+  });
+
+  useEffect(() => {
+    const observer = new ResizeObserver((entries) => {
+      if (entries[0]) {
+        const { width, height } = entries[0].contentRect;
+        setDimensions({ width, height });
+      }
+    });
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
+
+  return [ref, dimensions];
+}
+
+// Usage example:  const [divRef, dimensions] = useResizeObserver<HTMLDivElement>();
