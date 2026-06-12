ecc00496326a2b0b7cb0dd4dbbcb952921f0d62b
Salino3 <flaviobcn012@gmail.com>
2026-06-12 15:26:49 +0200

    save changes

--- src/common-app/submit-search-button/submit-search-button.test.tsx
+++ src/common-app/submit-search-button/submit-search-button.test.tsx
@@ -2,9 +2,25 @@ import { describe, it, expect, beforeEach, vi } from "vitest";
 import { render, screen, fireEvent } from "@testing-library/react";
 import { SubmitBasicBtn } from "./submit-search-button.component";
 
+vi.mock("react", async (importOriginal) => {
+  const actualReact = await importOriginal<typeof import("react")>();
+  return {
+    ...actualReact,
+  };
+});
+
 const renderComponent = (searchName = "") => {
   const utils = render(<SubmitBasicBtn searchName={searchName} />);
   return { ...utils };
 };
 
-describe("SubmitBasicBtn Component", () => {});
+describe("SubmitBasicBtn Component", () => {
+  it("Check when component is mounted, there is visible the text 'Search Character' and the magnifying icon on the button", () => {
+    renderComponent();
+
+    const textBtn = screen.queryByText("Search Character");
+
+    expect(textBtn).not.toBe(null);
+    expect(textBtn?.matches("Search Character"));
+  });
+});
